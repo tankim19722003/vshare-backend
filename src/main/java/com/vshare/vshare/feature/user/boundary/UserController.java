7abfda3cad6c67a13552073dce00ea631c45d164
Ngoc Tan Kim <kim.ngoctan@gmail.com>
2026-02-26 13:36:34 +0700

    add method to test rate limiting

--- src/main/java/com/vshare/vshare/feature/user/boundary/UserController.java
+++ src/main/java/com/vshare/vshare/feature/user/boundary/UserController.java
@@ -9,11 +9,7 @@ import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -35,5 +31,10 @@ public class UserController {
     public ApiResponse<UserLoginResponse> login(@Valid @RequestBody UserLoginRequest userLoginRequest) {
         log.info("login user: {}", userLoginRequest);
         return userService.login(userLoginRequest.getAccount(), userLoginRequest.getPassword());
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello world";
     }
 }
